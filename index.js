7a4f7d2e0ca96052c876481f6c907baad719623f
frogred8 <frogred8@gmail.com>
2019-12-13 10:10:38 +0900

    modify to fit heroku

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.get('/ping', function (req, res) {
   res.send('pong');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}`);
 });
