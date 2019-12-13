ab88efcd9372032f74c362d78808f642aabdcc4b
frogred8 <frogred8@gmail.com>
2019-12-13 11:42:09 +0900

    never sleep this app

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var http = require('http');
 var app = express();
 
 app.get('/ping', function (req, res) {
@@ -9,3 +10,7 @@ var PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+setInterval(function() {
+  http.get('http://heroku-test-jonghan.herokuapp.com/ping');
+}, 20 * 60 * 1000);
